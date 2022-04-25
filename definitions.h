7aebcf86d4ff196acb5c0f193d0f4221f18d3d01
Luan José de Almeida Cardoso <luan.jose.c@gmail.com>
2022-04-25 19:38:14 -0300

    Adiciona funções de controle

--- definitions.h
+++ definitions.h
@@ -2,7 +2,10 @@
 #include "semphr.h"
 SemaphoreHandle_t MutexContext;
 
+#define SIZE 3
 static uint8_t placer;
+
+static uint8_t controler[SIZE];
 
 void start_table()
 {
@@ -17,4 +20,19 @@ void set_value(uint8_t pointer)
 uint8_t get_valeu()
 {
     return placer;
+}
+
+void set_control(uint8_t place)
+{
+    controler[place]++;
+}
+
+uint8_t get_control(uint8_t place)
+{
+    return controler[place];
+}
+
+void reset_control(uint8_t place)
+{
+    controler[place] = 0;
 }
